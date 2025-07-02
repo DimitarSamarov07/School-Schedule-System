fbea7c720e5362c94517fb3d8e459b7925383ef8
AngelArabadzhiev08 <90851649+AngelArabadzhiev@users.noreply.github.com>
2025-07-02 09:06:14 +0300

    Upload Teacher.ts and Time.ts data models.

---
+++ data_models/Teacher.ts
@@ -0,0 +1,12 @@
+class Teacher {
+    id: number;
+    firstName: string;
+    lastName: string;
+
+    constructor(teacherId: number, firstName: string, lastName: string) {
+        this.id = teacherId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+}
---
+++ data_models/Time.ts
@@ -0,0 +1,23 @@
+class Time {
+    id: number;
+    startTime: Date;
+    endTime: Date;
+
+    constructor(timeId: number, startTime: string, endTime: string) {
+        let dateStart = new Date();
+        let dateEnd = new Date();
+
+        this.id = timeId;
+
+        //Split the received string and set it as the start time
+        let splitStart = startTime.split(":")
+        dateStart.setHours(parseInt(splitStart[0]), parseInt(splitStart[1]))
+        this.startTime = dateStart;
+
+        //Split the received string and set it as the end time
+        let splitEnd = endTime.split(":")
+        dateEnd.setHours(parseInt(splitEnd[0], parseInt(splitEnd[1])))
+        this.endTime = dateEnd;
+
+    }
+}
