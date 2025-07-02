f08d9b3796b9f49ce9cdda320c063b3371e6b0ca
komputer <gd1884719@gmail.com>
2025-07-02 09:21:00 +0300

    Upload Bell and Classes data models

---
+++ data_models/Bell.ts
@@ -0,0 +1,11 @@
+class Bell {
+    id: number;
+    name: string;
+    sound: Blob;
+
+    constructor(id: number, name: string, sound: Blob) {
+        this.id = id;
+        this.name = name;
+        this.sound = sound;
+    }
+}
