805de8316636222eed405c0e15375a4b1347e862
ViktorP2 <viktor.petkov@gikn.eu>
2025-07-02 09:29:20 +0300

    Upload Advertising.ts data model

---
+++ data_models/Advertising.ts
@@ -0,0 +1,12 @@
+﻿class Advertising
+{
+    id: number;
+    content: string;
+    imagePath: string;
+    
+ constructor(id: number , content: string, imagePath: string){
+     this.id = id;
+     this.content = content;
+     this.imagePath = imagePath;
+ }
+}
