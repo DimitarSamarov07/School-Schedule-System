ac4ea5f5e0d174923960a90ab1516531ccbaafef
Dimitar Samarov <samarovdimitar@gmail.com>
2025-07-03 08:50:59 +0300

    Create a database service with mock data and methods (yet to be implemented when the actual DB comes)

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 import express from "express";
+import {SqliteMaster as manager} from "./sqlite_services.js" ;
 
 const app = express(); // Initializing Express App
 let port = 6969;
 
+app.get("schedule", (req, res) => {
+    let {classId, date} = req.query;
+    res.send(manager.getScheduleByClassIdForDate(classId, date));
+})
 
-app.get("/test-page", (req, res)=>{
+app.get("/test-page", (req, res) => {
     res.send('Hello WORLD. ');
 });
 
-app.get("/random", (req, res)=>
-{
-    res.send(Math.random(0,100));
-}
+app.get("/random", (req, res) => {
+        res.send(Math.random(0, 100));
+    }
 );
 
-app.listen(port, ()=> console.log(`App Listening on port ${port}`));
+app.listen(port, () => console.log(`App Listening on port ${port}`));
