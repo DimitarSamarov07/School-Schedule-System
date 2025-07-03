c951b0743ba32b13512d7f6c6ac7cbca18c2f28b
AngelArabadzhiev08 <angelarabadzhiev82@gmail.com>
2025-07-03 11:40:29 +0300

    Upload Sqlite query for selecting schedules for each date.

---
+++ sqlite_consts.ts
@@ -0,0 +1,13 @@
+// TODO: SQLite query strings go here
+
+class Sqlite_consts {
+    static readonly SELECT_SCHEDULES_FOR_DATE = `SELECT cl.Name as 'className', c.Name as 'courseName', CONCAT(t.FName, ' ', t.LName) as 'fullTeacherName', r.Name as 'roomNumber', r.Floor as 'floor'
+                                                 FROM Schedule
+                                                          JOIN Dates d on D.D_ID = Schedule.D_ID
+                                                          JOIN Classes cl on Schedule.Class = cl.C_ID
+                                                          JOIN Courses c on c.C_ID = Schedule.Course
+                                                          JOIN Teachers t on t.T_ID = Schedule.T_ID
+                                                          JOIN Rooms r on r.R_ID = c.Room
+                                                 WHERE d.Date = unixepoch(?);`;
+
+}
