d9cd5d9b9d6d490a1873befefb82df9d24237e9c
Dimitar Samarov <samarovdimitar@gmail.com>
2025-07-04 11:04:52 +0300

    Fixup: Someone broke the branch(Viktor) Fix some code and undo the revert of some changes

--- data_models/Schedule.ts
+++ data_models/Schedule.ts
@@ -1,8 +1,9 @@
 import Course from "./Course.ts";
 import Class from "./Class.ts";
 import DateModel from "./DateModel.ts";
-import type Time from "./Time.js";
+import Time from "./Time.js";
 import Teacher from "./Teacher.js";
+import Room from "./Room.js";
 
 class Schedule {
     Class: Class;
@@ -10,11 +11,43 @@ class Schedule {
     Times: Time;
     Date: DateModel;
 
-    constructor(scheduleClass: Course, course: Class, times: Teacher, date: DateModel) {
+    constructor(scheduleClass: Class, course: Course, times: Time, date: DateModel) {
         this.Class = scheduleClass;
         this.Course = course;
         this.Times = times;
         this.Date = date;
+    }
+
+    static convertFromDBModel(schedule: any) {
+        let {
+            courseId,
+            courseName,
+            teacherId,
+            firstName,
+            lastName,
+            classId,
+            className,
+            classDesc,
+            dateId,
+            date,
+            isHoliday,
+            roomId,
+            roomName,
+            floor,
+            timeId,
+            startTime,
+            endTime
+        } = schedule;
+
+        let time = new Time(timeId, startTime, endTime);
+        let dateModel = new DateModel(dateId, date, isHoliday);
+        let teacher = new Teacher(teacherId, firstName, lastName);
+        let room = new Room(roomId, roomName, floor);
+        let course = new Course(courseId, courseName, teacher, room);
+        let classModel = new Class(classId, className, classDesc);
+
+        return new Schedule(classModel, course, time, dateModel);
+
     }
 }
 
