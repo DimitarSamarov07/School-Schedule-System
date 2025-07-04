db9064d0fad67403d972ec5821aa5978039d9535
AngelArabadzhiev08 <angelarabadzhiev82@gmail.com>
2025-07-04 12:25:54 +0300

    Added new queries

--- sqlite_constants.ts
+++ sqlite_constants.ts
@@ -1,7 +1,7 @@
 // TODO: SQLite query strings go here
 
 class SqliteConstants {
-    static readonly SELECT_SCHEDULES_FOR_DATE = `
+    static readonly BASE_SCHEDULE_QUERY = `
         SELECT c.id           as 'courseId',
                c.Name         as 'courseName',
                t.id           as 'teacherId',
@@ -27,10 +27,11 @@ class SqliteConstants {
                  JOIN Teachers t on t.id = Schedule.T_id
                  JOIN Rooms r on r.id = c.Room
                  JOIN Times ti on ti.id = Schedule.T_id
-        WHERE d.Date = (?);
-        `;
-    //YYYY.MM.DD
+    `;//YYYY.MM.DD
 
+    static readonly SELECT_SCHEDULES_FOR_DATE = `${this.BASE_SCHEDULE_QUERY} WHERE d.Date = (?);`;
+
+    static readonly SELECT_SCHEDULES_FOR_DATE_AND_CLASS_ID = `${this.BASE_SCHEDULE_QUERY} WHERE d.date = (?) AND c.id = (?)`
 
     static readonly SELECT_BELL_BY_NAME =
         `SELECT Id, Name, SoundPath
