9ca5fc5bf7ea9ece1e9df988765001fc0fb8c2a1
ViktorP2 <viktor.petkov@gikn.eu>
2025-07-07 09:05:03 +0300

    Add fetching for time and current class.

---
+++ Frontend/Fetchers/TimesFetcher.js
@@ -0,0 +1,17 @@
+﻿const fetchTime = async () => {
+    try {
+        const response = await fetch('http://localhost:6969/runningTime?Time');
+        const data = await response.json();
+        
+        // This will show us what properties we actually have
+        console.log('Available properties:', Object.keys(data));
+        
+        // This will show the full data object
+        console.log('Data:', data);
+        
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+fetchTime();
