58f1540e9e7ce7416ebe88b480a5b39d2e6a0e13
ViktorP2 <viktor.petkov@gikn.eu>
2025-07-08 11:45:57 +0300

    Finish the code for scheduleByClassIdAndDateFetcher.js

---
+++ Frontend/Fetchers/ScheduleByClassIdAndDateFetcher.js
@@ -0,0 +1,26 @@
+﻿async function fetchScheduleByClassIdForDate () {
+    try {
+        const classId = 1;
+        const fullDate = new Date();
+        //const date = `${fullDate.getFullYear()}-${fullDate.getMonth() + 1}-${fullDate.getDate()}`
+        const date = "2025-07-07";
+
+        const params = new URLSearchParams();
+        params.append("classId", classId);
+        params.append("date", date);
+
+
+        const response = await fetch(`http://localhost:6969/schedulesByClassIdForDate?${params}`);
+        const data = await response.json();
+        console.log('Data:', data);
+        console.log('classId');
+        return data;
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+fetchScheduleByClassIdForDate()
+
+export default fetchScheduleByClassIdForDate;
