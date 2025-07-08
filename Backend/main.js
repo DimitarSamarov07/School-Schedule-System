49d5f35d3b13a97093a82742ba95f8ac1f9d2dd5
Dimitar Samarov <samarovdimitar@gmail.com>
2025-07-08 12:13:52 +0300

    Add create endpoints

--- Backend/main.js
+++ Backend/main.js
@@ -1,8 +1,8 @@
 import express from "express";
-import manager from "./sqlite_services.js" ;
+import manager from "./sqlite_services.js";
 import moment from "moment";
 
- // Initializing Express App
+// Initializing Express App
 import helmet from "helmet";
 
 const app = express();
@@ -84,11 +84,124 @@ app.get("/date", async (req, res) => {
     res.send(result);
 })
 
-// Teachers
 
 // Create
-app.post("/teacher", async (req, res) => {
 
+app.post("/teacher", async (req, res) => {
+    let {firstName, lastName} = req.body;
+    if (firstName == null || lastName == null) {
+        res.status(406);
+        res.send("Malformed parameters");
+    }
+    let result = await manager.createTeacher(firstName, lastName).catch(err => {
+        res.status(500).send(err);
+    })
+    res.send(result);
+});
+
+app.post("/class", async (req, res) => {
+    let {name, description} = req.body;
+    if (name == null || description == null) {
+        res.status(406);
+        res.send("Malformed parameters");
+    }
+    let result = await manager.createClass(name, description).catch(err => {
+        res.status(500).send(err);
+    })
+
+    res.status(200)
+})
+
+app.post("/room", async (req, res) => {
+    let {name, floor} = req.body;
+    if (name == null || floor == null) {
+        res.status(406);
+        res.send("Malformed parameters");
+    }
+    let result = await manager.createRoom(name, floor).catch(err => {
+        res.status(500).send(err);
+    })
+
+    res.send(result);
+})
+
+app.post("/time", async (req, res) => {
+    let {start, end} = req.body;
+    if (start == null || end == null) {
+        res.status(406);
+        res.send("Malformed parameters");
+    }
+    let result = await manager.createTime(start, end).catch(err => {
+        res.status(500).send(err);
+    })
+
+    res.send(result);
+})
+
+app.post("/advertising", async (req, res) => {
+    let {text, imagePath} = req.body;
+    if (text == null || link == null) {
+        res.status(406);
+        res.send("Malformed parameters");
+    }
+    let result = await manager.createAdvertising(text, imagePath).catch(err => {
+        res.status(500).send(err);
+    })
+
+    res.send(result);
+})
+
+app.post("/bell", async (req, res) => {
+    let {name, soundPath} = req.body;
+    if (name == null || soundPath == null) {
+        res.status(406);
+        res.send("Malformed parameters");
+    }
+    let result = await manager.createBell(name, soundPath).catch(err => {
+        res.status(500).send(err);
+    })
+
+    res.send(result);
+})
+
+app.post("/schedule", async (req, res) => {
+    let {courseId, classId, timeId, dateId} = req.body;
+    if (courseId == null || classId == null || timeId == null || dateId == null) {
+        res.status(406);
+        res.send("Malformed parameters");
+    }
+    let result = await manager.createSchedule(courseId, classId, timeId, dateId).catch(err => {
+        res.status(500).send(err);
+    })
+
+    res.send(result);
+})
+
+app.post("/course", async (req, res) => {
+    let {name, teacherId, roomId} = req.body;
+    if (name == null || teacherId == null || roomId == null) {
+        res.status(406);
+        res.send("Malformed parameters");
+    }
+    let result = await manager.createCourse(name, teacherId, roomId).catch(err => {
+        res.status(500).send(err);
+    })
+
+    res.send(result);
+})
+
+app.post("/date", async (req, res) => {
+    let {date, isHoliday} = req.body;
+
+    if (date == null || isHoliday == null || typeof isHoliday !== "boolean" || moment(date, 'YYYY-MM-DD').isValid() === false) {
+        res.status(406);
+        res.send("Malformed parameters");
+    }
+    let result = await manager.createDate(date, isHoliday).catch(err => {
+        res.status(500).send(err);
+    })
+
+    res.send(result);
 })
 
 manager.initializeConnection();
