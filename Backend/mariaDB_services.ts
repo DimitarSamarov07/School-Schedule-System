dfd1e78fb691cc7b136f534e56ae214fbdd1cccf
DimitarSamarov07 <samarovdimitar@gmail.com>
2026-01-29 16:58:18 +0200

    Add dotenv. Fix package.json

--- Backend/mariaDB_services.ts
+++ Backend/mariaDB_services.ts
@@ -1,15 +1,16 @@
-import mariadb from 'mariadb';
+import mariadb, {type Pool} from 'mariadb';
 import dotenv from 'dotenv';
 import MariaDB_constants from "./mariaDB_constants.ts";
 
 class MariaDBServices {
-    static pool ;
+    static pool: Pool;
+
     static initializeConnection(): void {
         dotenv.config();
         this.pool = mariadb.createPool({
-            host: 'localhost',
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
+            host: process.env.DATABASE_HOST,
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
             connectionLimit: 5,
             trace: true
         });
