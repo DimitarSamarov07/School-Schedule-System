eccf6d55e80cccb62abc68574712a1b670e5f56f
AngelArabadzhiev <angelarabadzhiev82@gmail.com>
2026-02-15 12:38:56 +0200

    Extended `SchoolProvider` context with `username` and `email`; localized `UserDropdown` component with Bulgarian translations and dynamic user data display.

--- FrontEnd/providers/SchoolProvider.tsx
+++ FrontEnd/providers/SchoolProvider.tsx
@@ -1,4 +1,3 @@
-// context/SchoolProvider.tsx
 "use client";
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
@@ -9,28 +8,30 @@ const SchoolContext = createContext<any>(null);
 export const SchoolProvider = ({ children }: { children: React.ReactNode }) => {
     const [currentSchool, setCurrentSchool] = useState<any>(null);
     const [accessList, setAccessList] = useState([]);
+    const [username, setUsername] = useState([]);
+    const [email, setEmail] = useState([]);
 
     useEffect(() => {
         const token = Cookies.get('AUTH_TOKEN');
         if (token) {
             const decoded: any = jwtDecode(token);
             setAccessList(decoded.accessList);
+            setUsername(decoded.username);
+            setEmail(decoded.email);
 
-            // Default to the first school in the list if none is selected
             if (decoded.accessList?.length > 0) {
                 setCurrentSchool(decoded.accessList[0]);
             }
         }
     }, []);
 
-    // Function to switch schools (if the user has multiple)
     const switchSchool = (id: number) => {
         const school = accessList.find((a: any) => a.SchoolId === id);
         if (school) setCurrentSchool(school);
     };
 
     return (
-        <SchoolContext.Provider value={{ currentSchool, accessList, switchSchool }}>
+        <SchoolContext.Provider value={{ currentSchool,username, email, accessList, switchSchool }}>
     {children}
     </SchoolContext.Provider>
 );
